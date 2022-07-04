327a52c6f70869909034383756bc905162e886cc
Zion Miller <djzionmiller@gmail.com>
2022-07-03 21:58:06 -0700

    commit tree b5e1cedfd3c0932a1f471fd193442d804c35bb29

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (driverArray) => {
+    // return first two of the array
+    // if array of 10 drivers, how to return just the
+    return driverArray.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (driverArray) => {
+    return driverArray.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num){
+    return function (number){
+       return number * num
+    }
+};
+
+function fareDoubler(num) {
+    return num * 2
+};
+
+function fareTripler(num) {
+    return num * 3
+};
+
+function selectDifferentDrivers(arrayOfDrivers, func){
+   return func(arrayOfDrivers);
+}
